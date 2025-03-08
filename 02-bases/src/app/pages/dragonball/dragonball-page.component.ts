4a4f3af034b02b04396ddf07e21e4a028f6239fc
Mariano Luis Arias <mariano.arias@neoris.com>
2025-03-08 11:05:56 -0300

    Section 5: Classes CSS de forma condicional

--- 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 @Component({
   selector: 'app-dragonball-page',
   imports: [],
@@ -8,4 +13,24 @@ import { Component } from '@angular/core';
 })
 export class DragonballPageComponent {
 
+  characters = signal<Character[]>([
+    {
+      id: 1, name: 'Goku', power: 123
+    },
+    {
+      id: 2, name: 'Batman', power: 555
+    }, 
+    {
+      id: 3, name: 'Mazinger', power: 434
+    }, 
+    {
+      id: 4, name: 'Sandokan', power: 999
+    }
+  ]);
+
+  // powerClasses = computed(()=>{
+  //   return {
+  //     'text-danger':true
+  //   }
+  // })
 }
