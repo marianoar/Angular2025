bc82ceaf79a41cec5fc60b8568aaeecc53fc1477
Mariano Arias <arias.mariano.ar@gmail.com>
2025-04-01 19:21:51 -0300

    Pipes i18nSelect

--- 05-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Raviol',
+  gender: 'male',
+  age: 33,
+  address: 'CABA, Argentina'
+}
+const client2 = {
+  name: 'Analia',
+  gender: 'female',
+  age: 35,
+  address: 'Sarasa, Argentina'
+}
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
 
+  //i18n Select
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient() {
+
+    if (this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+    this.client.set(client1);
+  }
 }
