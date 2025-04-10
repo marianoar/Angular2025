d3da36fe84f31c465e5e13cbae05756777a61949
Mariano Arias <arias.mariano.ar@gmail.com>
2025-04-10 17:20:32 -0300

    ReactiveForms: FormGroup, FormControl

--- 06-reactive-forms/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 06-reactive-forms/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,20 @@
 import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
   styleUrl: './basic-page.component.css'
 })
 export class BasicPageComponent {
+
+  myForm = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(0),
+    inStorage: new FormControl(0),
+  }) // los inicializamos para el tipado
+
 
 }
