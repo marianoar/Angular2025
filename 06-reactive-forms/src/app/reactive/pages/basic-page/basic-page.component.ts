2b57807fabff702e19c69a43aa1d75ba8dcf45e5
Mariano Arias <arias.mariano.ar@gmail.com>
2025-04-11 10:00:50 -0300

    Form Builder et Validators

--- 06-reactive-forms/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 06-reactive-forms/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,20 +1,25 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
   imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './basic-page.component.html',
-  styleUrl: './basic-page.component.css'
 })
 export class BasicPageComponent {
 
-  myForm = new FormGroup({
-    name: new FormControl(''),
-    price: new FormControl(0),
-    inStorage: new FormControl(0),
-  }) // los inicializamos para el tipado
+  // myForm = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // }) // los inicializamos para el tipado
 
-
+  private fb = inject(FormBuilder);
+  myForm = this.fb.group({
+    //name: ['', Validadores sincronos[], validadores asincronos[] ]
+    name: ['', [Validators.required, Validators.minLength(3)],[]],
+    price:[, [Validators.required, Validators.min(10)],],
+    inStorage: [0, [Validators.required, Validators.min(0)],]
+  });
 }
