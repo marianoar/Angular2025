5f4c7e4740f7d5bb3062f88eb005db683da05a14
Mariano Arias <arias.mariano.ar@gmail.com>
2025-04-13 22:30:26 -0300

    Reactive forms: controls, validaciones y errores por pantalla

--- 06-reactive-forms/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 06-reactive-forms/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -16,10 +16,42 @@ export class BasicPageComponent {
   // }) // los inicializamos para el tipado
 
   private fb = inject(FormBuilder);
-  myForm = this.fb.group({
+  myForm: FormGroup = this.fb.group({
     //name: ['', Validadores sincronos[], validadores asincronos[] ]
-    name: ['', [Validators.required, Validators.minLength(3)],[]],
-    price:[, [Validators.required, Validators.min(10)],],
+    name: ['', [Validators.required, Validators.minLength(3)], []],
+    price: [, [Validators.required, Validators.min(10)],],
     inStorage: [0, [Validators.required, Validators.min(0)],]
   });
+
+  isValidFile(fieldName: string): boolean | null {
+    return (this.myForm.controls[fieldName].errors &&
+            this.myForm.controls[fieldName].touched);
+  }
+
+  getFieldError(fieldName: string): string | null {
+    if (!this.myForm.controls[fieldName])
+      return null;
+
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo de ${errors['minlength'].requiredLength} caracteres`;
+        case 'min':
+          return `Valor minimo de ${errors['min'].min}`;
+      }
+    }
+    return null;
+  }
+
+  onSave(){
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    this.myForm.reset(); // aca se pueden re setear los valores po default
+  }
 }
