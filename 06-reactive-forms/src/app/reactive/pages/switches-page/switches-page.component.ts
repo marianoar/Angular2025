064bcd2fa1611b739457ff69f728436d9ce90620
Mariano Arias <arias.mariano.ar@gmail.com>
2025-04-15 10:19:03 -0300

    Section 15: ReactiveForms, switches, checks and radio buttons

--- 06-reactive-forms/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 06-reactive-forms/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,12 +1,29 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.component.html',
   styleUrl: './switches-page.component.css'
 })
 export class SwitchesPageComponent {
 
+
+  private fb = inject(FormBuilder);
+  formUtils = FormUtils;
+  
+  myForm: FormGroup = this.fb.group(
+    {
+      gender:[undefined, Validators.required],
+      notifications: [true],
+      termsAndConditions:[false, Validators.requiredTrue]
+    }
+  )
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+    }
 }
