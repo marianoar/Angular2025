b8fc58d6aaef3f8a67decdfee53857bb7b604f1e
Mariano Arias <arias.mariano.ar@gmail.com>
2025-04-18 17:04:25 -0300

    Reactive Forms Selectores anidados inicio

---
+++ 06-reactive-forms/src/app/country/service/country.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { Country } from "../interfaces/country.interface";
+
+@Injectable({providedIn:'root'})
+export class CountryService{
+
+    private baseUrl= 'https://restcountries.com/v3.1'
+    private http = inject(HttpClient);
+
+    private _regions =
+    [
+        'Africa',
+        'Americas',
+        'Asia',
+        'Europe',
+        'Oceania'
+    ];
+
+    get regions():string[]{
+        return [...this._regions];
+    }
+
+    getCountriesByRegion(region:string): Observable<Country[]>{
+        if(!region)
+            return of([]);
+        const url = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
+
+        return this.http.get<Country[]>(url);
+    }
+
+    getCountryByAlphaCode(alphaCode: string):Observable<Country>{
+        const url = `${this.baseUrl}/region/${alphaCode}?fields=cca3,name,borders`;
+
+        return this.http.get<Country>(url);
+    }
+
+    getCountryBordersByCode(borders: string[]){
+        
+    }
+}
