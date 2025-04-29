4e9388858b4809614f6db29c5ec085fe3ae6c58e
Mariano Arias <arias.mariano.ar@gmail.com>
2025-04-29 19:41:32 -0300

    Section 18 inicio problems varios: fonts, animation, alias, backend

--- 09-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
+++ 09-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ProductCardComponent } from "../product-page/product-card/product-card.component";
+
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent { }
