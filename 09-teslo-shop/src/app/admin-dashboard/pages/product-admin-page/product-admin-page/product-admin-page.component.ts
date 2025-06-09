780262865a9581837a21d1eae838babe7b7874ce
Mariano Arias <arias.mariano.ar@gmail.com>
2025-06-08 22:18:08 -0300

    details product page by id

--- 09-teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-admin-page/product-admin-page.component.ts
+++ 09-teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-admin-page/product-admin-page.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs';
+import { ProductService } from 'src/app/products/services/products.service';
 
 @Component({
   selector: 'app-product-admin-page',
   imports: [],
   templateUrl: './product-admin-page.component.html',
 })
-export class ProductAdminPageComponent { }
+export class ProductAdminPageComponent {
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+  productService = inject(ProductService);
+
+  productId = toSignal(
+    this.activatedRoute.params.pipe(map((params) => params['id']))
+  );
+
+  productResource = rxResource({
+    request: () => ({ id: this.productId() }),
+    loader: ({ request }) => {
+      return this.productService.getProductById(request.id);
+    },
+  });
+
+  redirectEffect = effect(() => {
+    if (this.productResource.error()) {
+      this.router.navigate(['/admin/products']);
+    }
+  });
+}
--- 09-teslo-shop/src/app/products/services/products.service.ts
+++ 09-teslo-shop/src/app/products/services/products.service.ts
@@ -54,8 +54,19 @@ export class ProductService {
     }
 
     return this.http.get<Product>(`${baseUrl}/products/${idSlug}`).pipe(
-      delay(1500),
+      // delay(1500),
       tap((product) => this.productCache.set(idSlug, product))
+    );
+  }
+
+  getProductById(id: string): Observable<Product> {
+    if (this.productCache.has(id)) {
+      return of(this.productCache.get(id)!);
+    }
+
+    return this.http.get<Product>(`${baseUrl}/products/${id}`).pipe(
+      // delay(1500),
+      tap((product) => this.productCache.set(id, product))
     );
   }
 }
